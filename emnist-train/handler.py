5de10ec212ccd022208a6c8c15193577ae563194
Eduardo Janicas <edujanicas@icloud.com>
2018-07-17 17:11:17 +0100

    First implementation of Downpour SGD

--- emnist-train/handler.py
+++ emnist-train/handler.py
@@ -63,7 +63,7 @@ def handle(req):
         req (str): request body
     """
     # Print next line for debug only
-    sys.stderr.write(str(os.environ))
+    # sys.stderr.write(str(os.environ))
 
     client = base.Client((os.getenv("MEMCACHED_SERVICE_HOST"),
                           int(os.getenv("MEMCACHED_SERVICE_PORT"))))
@@ -96,11 +96,21 @@ def handle(req):
     images, labels, test_images, test_labels = load_dataset(
         worker_id, number_of_workers)
 
-    for iter in range(iterations):
+    while True:
 
         correct_cnt = 0
 
         for i in range(int(len(images) / batch_size)):
+
+            weights_0_1 = np.frombuffer(
+                client.get('weights_0_1')).reshape(pixels_per_image,
+                                                   hidden_size)
+            weights_1_2 = np.frombuffer(
+                client.get('weights_1_2')).reshape(hidden_size, num_labels)
+
+            weights_0_1.flags.writeable = True
+            weights_1_2.flags.writeable = True
+
             batch_start, batch_end = ((i * batch_size), ((i + 1) * batch_size))
             # 2. PREDICT & COMPARE: Make a Prediction,
             #    Calculate Output Error and Delta
@@ -129,6 +139,9 @@ def handle(req):
             weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
             weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
 
+            client.set('weights_0_1', weights_0_1.tobytes())
+            client.set('weights_1_2', weights_1_2.tobytes())
+
         test_correct_cnt = 0
         for i in range(len(test_images)):
             layer_0 = test_images[i:i + 1]
@@ -141,21 +154,5 @@ def handle(req):
                          str(test_correct_cnt / float(len(test_images))) +
                          " Train-Acc:" + str(correct_cnt / float(len(images))))
 
-        if client.get('updated') == 0:
-            client.set('weights_0_1', weights_0_1.tobytes())
-            client.set('weights_1_2', weights_1_2.tobytes())
-            client.set('updated', 1)
-        else:
-            weights_0_1_global = np.frombuffer(
-                client.get('weights_0_1')).reshape(pixels_per_image,
-                                                   hidden_size)
-            weights_1_2_global = np.frombuffer(
-                client.get('weights_1_2')).reshape(hidden_size, num_labels)
-
-            a_0_1 = np.array([weights_0_1, weights_0_1_global])
-            a_1_2 = np.array([weights_1_2, weights_1_2_global])
-            averaged_0_1 = np.average(a_0_1, axis=0)
-            averaged_1_2 = np.average(a_1_2, axis=0)
-
-            client.set('weights_0_1', averaged_0_1.tobytes())
-            client.set('weights_1_2', averaged_1_2.tobytes())
+        if (test_correct_cnt / float(len(test_images))) > 0.75:
+            return
